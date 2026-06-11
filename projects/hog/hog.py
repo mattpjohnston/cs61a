c1fe3f1f358376ffc5bae0d03edf8c9a4e90f2e0
mattpjohnston <matthewjohnston89@sky.com>
2026-06-11 23:07:57 +0100

    complete hog: problem 10

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -330,7 +330,10 @@ def boar_strategy(score, opponent_score, threshold=11, num_rolls=6):
     points, and returns NUM_ROLLS otherwise. Ignore the Sus Fuss rule.
     """
     # BEGIN PROBLEM 10
-    return num_rolls  # Remove this line once implemented.
+    boar_roll = boar_brawl(score, opponent_score)
+    if boar_roll >= threshold:
+        return 0
+    return num_rolls
     # END PROBLEM 10
 
 
